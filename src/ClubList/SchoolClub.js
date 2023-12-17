95494e85cb9c57713025d7bc55f526538caa5097
dyun23 <ozsally@naver.com>
2023-12-18 04:48:38 +0900

    사진 추가

--- src/ClubList/SchoolClub.js
+++ src/ClubList/SchoolClub.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import * as Components from '../Components';
 import 'react-slideshow-image/dist/styles.css'
-import slide1 from '../img/slide1.png';
 import slide2 from '../img/slide2.png';
+import slide4 from '../img/slide4.jpg';
+import slide5 from '../img/slide5.jpg';
+import slide6 from '../img/slide6.jpg';
 import { useNavigate } from 'react-router-dom';
 
 const SchoolClub = (props) => {
-    const slideshowImg = [slide1, slide2];
-    const clubList = [{img: "https://images.unsplash.com/photo-1493863641943-9b68992a8d07?q=80&w=2058&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "사진동아리",info:"SEPE 신입생 모집"},
-    {img: "https://images.unsplash.com/photo-1548123378-bde4eca81d2d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "댄스 동아리",info:"신바람 2023년 신입생 공고"},
-    {img: "https://images.unsplash.com/photo-1493863641943-9b68992a8d07?q=80&w=2058&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "사진동아리",info:"SEPE 신입생 모집"},
-    {img: "https://images.unsplash.com/photo-1548123378-bde4eca81d2d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "댄스 동아리",info:"신바람 2023년 신입생 공고"}]
+    const slideshowImg = [slide2, slide4, slide5, slide6];
+    const clubList = [{img: "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "코딩 동아리",info:"CodeLab 3기 모집"},
+    {img: "https://images.unsplash.com/photo-1630609674924-1b381d09d313?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "친목 동아리",info:"Moments 신규 모집합니다"},
+    {img: "https://images.unsplash.com/photo-1509343256512-d77a5cb3791b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "디자인 동아리",info:"컬러풀 드림즈 신규 모집"},
+    {img: "https://images.unsplash.com/photo-1475440197469-e367ec8eeb19?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "스포츠 동아리",info:"HOMERUN 야구 보러 갈 사람 모여라"},
+    {img: "https://images.unsplash.com/photo-1616680214084-22670de1bc82?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "봉사 동아리",info:"'순수' 하게 봉사할 신입생 모집"},
+    {img: "https://images.unsplash.com/photo-1506880018603-83d5b814b5a6?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "독서 동아리",info:"책장여행 부원 모집"},
+    {img: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "창업 동아리",info:"폴라리스 대환영"},
+    {img: "https://images.unsplash.com/photo-1514050566906-8d077bae7046?q=80&w=1173&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "스포츠 동아리",info:"KickHeroes 신입생 공고"},
+    {img: "https://images.unsplash.com/photo-1490481651871-ab68de25d43d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "패션 동아리",info:"스타일리스트 동아리 모집"},
+    {img: "https://images.unsplash.com/photo-1515669097368-22e68427d265?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "맛집 동아리",info:"Flavor과 함께해요"},
+    {img: "https://images.unsplash.com/flagged/photo-1567116681178-c326fa4e2c8b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "봉사 동아리",info:"유사모 신규 모집"},
+    {img: "https://images.unsplash.com/photo-1535525153412-5a42439a210d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "댄스 동아리",info:"FeelMyRhythm 6기 모집"},
+    {img: "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "코딩 동아리",info:"CodeLab 3기 모집"},
+    {img: "https://images.unsplash.com/photo-1630609674924-1b381d09d313?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "친목 동아리",info:"Moments 신규 모집합니다"},
+    {img: "https://images.unsplash.com/photo-1509343256512-d77a5cb3791b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "디자인 동아리",info:"컬러풀 드림즈 신규 모집"},
+    {img: "https://images.unsplash.com/photo-1475440197469-e367ec8eeb19?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "스포츠 동아리",info:"HOMERUN 야구 보러 갈 사람 모여라"},
+    {img: "https://images.unsplash.com/photo-1616680214084-22670de1bc82?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "봉사 동아리",info:"'순수' 하게 봉사할 신입생 모집"},
+    {img: "https://images.unsplash.com/photo-1506880018603-83d5b814b5a6?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "독서 동아리",info:"책장여행 부원 모집"},
+    {img: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "창업 동아리",info:"폴라리스 대환영"},
+    {img: "https://images.unsplash.com/photo-1514050566906-8d077bae7046?q=80&w=1173&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "스포츠 동아리",info:"KickHeroes 신입생 공고"},
+    {img: "https://images.unsplash.com/photo-1490481651871-ab68de25d43d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "패션 동아리",info:"스타일리스트 동아리 모집"},
+    {img: "https://images.unsplash.com/photo-1515669097368-22e68427d265?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "맛집 동아리",info:"Flavor과 함께해요"},
+    {img: "https://images.unsplash.com/flagged/photo-1567116681178-c326fa4e2c8b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "봉사 동아리",info:"유사모 신규 모집"},
+    {img: "https://images.unsplash.com/photo-1535525153412-5a42439a210d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "댄스 동아리",info:"FeelMyRhythm 6기 모집"}]
     
     const navigate = useNavigate();
 
--- src/ClubList/UnionClub.js
+++ src/ClubList/UnionClub.js
@@ -2,19 +2,35 @@ import React from 'react';
 import * as Components from '../Components';
 import 'react-slideshow-image/dist/styles.css'
 import slide1 from '../img/slide1.png';
-import slide2 from '../img/slide2.png';
+import slide3 from '../img/slide3.jpg';
 import { useNavigate } from 'react-router-dom';
 
 const UnionClub = (props) => {
-    const slideshowImg = [slide1, slide2];
-    const clubList = [{img: "https://images.unsplash.com/photo-1493863641943-9b68992a8d07?q=80&w=2058&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "사진동아리",info:"SEPE 신입생 모집"},
+    const slideshowImg = [slide1, slide3];
+    const clubList = [{img: "https://images.unsplash.com/photo-1493863641943-9b68992a8d07?q=80&w=2058&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "사진 동아리",info:"SEPE 신입생 모집"},
     {img: "https://images.unsplash.com/photo-1548123378-bde4eca81d2d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "댄스 동아리",info:"신바람 2023년 신입생 공고"},
-    {img: "https://images.unsplash.com/photo-1493863641943-9b68992a8d07?q=80&w=2058&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "사진동아리",info:"SEPE 신입생 모집"},
+    {img: "https://images.unsplash.com/photo-1529422643029-d4585747aaf2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "스포츠 동아리",info:"사이클러 3기 동아리원 모집"},
+    {img: "https://images.unsplash.com/photo-1526772662000-3f88f10405ff?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "등산 동아리",info:"PeakQuest와 함께하실 분"},
+    {img: "https://images.unsplash.com/photo-1455390582262-044cdead277a?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "글쓰기 동아리",info:"글잡이 동아리 모집"},
+    {img: "https://images.unsplash.com/photo-1556911220-e15b29be8c8f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "요리 동아리",info:"TasteVoyage에서 만나요"},
+    {img: "https://images.unsplash.com/photo-1598899134739-24c46f58b8c0?q=80&w=2056&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "영화 동아리",info:"시네마포차 영업합니다"},
+    {img: "https://images.unsplash.com/photo-1523975864490-174dd4d9a41e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "여행 동아리",info:"트레버 부원 모집"},
+    {img: "https://images.unsplash.com/photo-1529699211952-734e80c4d42b?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "체스 동아리",info:"King'sGame 신입생 모집"},
+    {img: "https://images.unsplash.com/photo-1495175448924-1d9a30c90a42?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "음악 동아리",info:"SoundDiary 부원 모집"},
+    {img: "https://images.unsplash.com/photo-1504966981333-1ac8809be1ca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "천문학 동아리",info:"별의 여정 어떠세요?"},
+    {img: "https://images.unsplash.com/photo-1531746790731-6c087fecd65a?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "로봇 동아리",info:"로봇 워커스 모집"},
+{img: "https://images.unsplash.com/photo-1493863641943-9b68992a8d07?q=80&w=2058&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "사진 동아리",info:"SEPE 신입생 모집"},
     {img: "https://images.unsplash.com/photo-1548123378-bde4eca81d2d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "댄스 동아리",info:"신바람 2023년 신입생 공고"},
-    {img: "https://images.unsplash.com/photo-1493863641943-9b68992a8d07?q=80&w=2058&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "사진동아리",info:"SEPE 신입생 모집"},
-    {img: "https://images.unsplash.com/photo-1548123378-bde4eca81d2d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "댄스 동아리",info:"신바람 2023년 신입생 공고"},
-    {img: "https://images.unsplash.com/photo-1493863641943-9b68992a8d07?q=80&w=2058&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "사진동아리",info:"SEPE 신입생 모집"},
-    {img: "https://images.unsplash.com/photo-1548123378-bde4eca81d2d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "댄스 동아리",info:"신바람 2023년 신입생 공고"}]
+    {img: "https://images.unsplash.com/photo-1529422643029-d4585747aaf2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "스포츠 동아리",info:"사이클러 동아리원 모집"},
+    {img: "https://images.unsplash.com/photo-1526772662000-3f88f10405ff?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "등산 동아리",info:"PeakQuest와 함께하실 분"},
+    {img: "https://images.unsplash.com/photo-1455390582262-044cdead277a?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "글쓰기 동아리",info:"글잡이 동아리 모집"},
+    {img: "https://images.unsplash.com/photo-1556911220-e15b29be8c8f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "요리 동아리",info:"TasteVoyage에서 만나요"},
+    {img: "https://images.unsplash.com/photo-1598899134739-24c46f58b8c0?q=80&w=2056&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "영화 동아리",info:"시네마포차 영업합니다"},
+    {img: "https://images.unsplash.com/photo-1523975864490-174dd4d9a41e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "여행 동아리",info:"트레버 부원 모집"},
+    {img: "https://images.unsplash.com/photo-1529699211952-734e80c4d42b?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "체스 동아리",info:"King'sGame 신입생 모집"},
+    {img: "https://images.unsplash.com/photo-1495175448924-1d9a30c90a42?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "음악 동아리",info:"SoundDiary 부원 모집"},
+    {img: "https://images.unsplash.com/photo-1504966981333-1ac8809be1ca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "천문학 동아리",info:"별의 여정 어떠세요?"},
+    {img: "https://images.unsplash.com/photo-1531746790731-6c087fecd65a?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", title: "로봇 동아리",info:"로봇 워커스 모집"}]
     
     const navigate = useNavigate();
 
